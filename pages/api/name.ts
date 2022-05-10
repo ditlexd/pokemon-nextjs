30bacd6184524c854a05c1e4076b91e4ad1f4074
Ditlexd <ditlef.diseth@gmail.com>
2022-05-10 17:20:05 +0200

    Add error handling api/name

--- pages/api/name.ts
+++ pages/api/name.ts
@@ -6,12 +6,29 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
+  if (!req.query.id) {
+    res.status(400).end();
+    return;
+  }
+
   const { id } = req.query;
 
-  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(
-    (res) => res.json(),
-  );
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
 
-  const pokemonName = pokemon.name.split('-')[0];
-  res.status(200).json({ name: pokemonName });
+    if (!response.ok) {
+      throw new Error(
+        `Error with status: ${response.status.toString()} and status text ${
+          response.statusText
+        }`,
+      );
+    }
+
+    const pokemon = await response.json();
+    const pokemonName = pokemon.name.split('-')[0];
+    res.status(200).json({ name: pokemonName });
+  } catch (e) {
+    console.log('Error', e);
+    throw e;
+  }
 }
